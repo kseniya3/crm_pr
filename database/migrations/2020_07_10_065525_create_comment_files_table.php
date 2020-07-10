5bdce7703a98f6e1431427c1d7e9d012c65bec11
kseniya <kozlovakseniya3@yandex.ru>
2020-07-10 14:06:30 +0700

    added table Deals and Comment_files

---
+++ database/migrations/2020_07_10_065525_create_comment_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->string('file_path');
+            $table->bigInteger('comment_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_files');
+    }
+}
