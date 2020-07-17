daaaa8b5a31123f247470012871a69ee264d7515
kseniya <kozlovakseniya3@yandex.ru>
2020-07-17 12:57:23 +0700

    start vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <script src="{{asset('js/app.js')}}"></script>
-<div class="container">
+<div id="vue-id" class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
+                    @if (session('status'))]
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
@@ -19,10 +19,13 @@
                     <a href="{{route('clients.show_clients')}}">Client</a>
                     <a href="{{route('users.show_user')}}">User</a>
                     {{ __('You are logged in!') }}
-                        <div id="app"><example-component>hoii</example-component></div>
+                        <div><example-component></example-component></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+@push('js')
+    <script src="{{asset('js/app.js')}}"></script>
+@endpush
