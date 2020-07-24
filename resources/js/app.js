b0705f7deede7c34a4f8626a113b404d4503a0fd
AmelK <af5909704@gmail.com>
2020-07-24 10:09:03 +0700

    doompdf

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,15 +24,4 @@ Vue.component('comment-vue', require('./components/CommentComponent.vue').defaul
 
 const v = new Vue({
     el: '#vue-id',
-<<<<<<< HEAD
 });
-=======
-});
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
->>>>>>> d2cd08ec3f3373803f1ef3a33ff90d922a14b191
