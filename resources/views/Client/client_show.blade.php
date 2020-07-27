c977270712d5acd06cb5a28d4336a38ae97de6f9
kseniya <kozlovakseniya3@yandex.ru>
2020-07-27 13:34:12 +0700

    aaddd commit

--- resources/views/Client/client_show.blade.php
+++ resources/views/Client/client_show.blade.php
@@ -18,10 +18,13 @@
                     </div>
                     @endif
             </div>
-            <div class="box-body">
-        <div class="row">
-            <div class="col-sm-12">
-                    <table class="table table-bordered table-hover dataTable" role="grid">
+        </div>
+
+        <div class="box-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                    <table class="table">
                         <thead>
                         <tr role="row" class="box-title">
                             <th scope="col">№</th>
@@ -36,53 +39,48 @@
                             <th></th>
                         </tr>
                         </thead>
-                <div class="box-body no-paddin">
-                    <form action="{{route('clients.find_client')}}" method="post">
-                    @csrf
-                    <input type="text" name="find" id="find" class="form-control">
-                    <button type="submit" class="btn btn-primary">Найти</button>
-                    </form>
-
-                        <tbody>
-                        @foreach($items as $item)
-                            <tr>
-                                <td>{{$item->id}}</td>
-                                <td>{{$item->second_name}}</td>
-                                <td>{{$item->first_name}}</td>
-                                <td>{{$item->middle_name}}</td>
-                                <td>{{$item->contacts_telephone}}</td>
-                                <td>{{$item->contacts_email}}</td>
-                                <td>{{$item->description}}</td>
-                                <td>{{$item->company_name}}</td>
-                                <td>{{ $item->deals()->pluck('deal_name')->implode(', ')}}</td>
-                                <td class="table-buttons">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-outline-danger dropdown-toggle" data-toggle="dropdown" aria-hasopup="true" aria-expended="false">Actions</button>
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li>
-                                            <form method="POST" action="{{route('clients.delete_client',$item->id)}}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="dropdown-item">Delete</button>
-                                            </form>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('clients.update_client_str',$item->id)}}" class="dropdown-item">Edit</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('clients.show_report',$item->id)}}" class="dropdown-item">Report</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                </td>
-                            </tr>
+                        <div class="box-body no-paddin">
+                            <tbody>
+                            @foreach($items as $item)
+                                <tr>
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->second_name}}</td>
+                                    <td>{{$item->first_name}}</td>
+                                    <td>{{$item->middle_name}}</td>
+                                    <td>{{$item->contacts_telephone}}</td>
+                                    <td>{{$item->contacts_email}}</td>
+                                    <td>{{$item->description}}</td>
+                                    <td>{{$item->company_name}}</td>
+                                    <td>{{ $item->deals()->pluck('deal_name')->implode(', ')}}</td>
+                                    <td class="table-buttons">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-outline-danger dropdown-toggle" data-toggle="dropdown" aria-hasopup="true" aria-expended="false">Actions</button>
+                                            <ul class="dropdown-menu" role="menu">
+                                                <li>
+                                                    <form method="POST" action="{{route('clients.delete_client',$item->id)}}">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="dropdown-item">Delete</button>
+                                                    </form>
+                                                </li>
+                                                <li>
+                                                    <a href="{{route('clients.update_client_str',$item->id)}}" class="dropdown-item">Edit</a>
+                                                </li>
+                                                <li>
+                                                    <a href="{{route('clients.show_report',$item->id)}}" class="dropdown-item">Report</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                </tr>
                             @endforeach
-                        </tbody>
+                            </tbody>
+                        </div>
                     </table>
+                    </div>
                 </div>
                 {{$items->links()}}
             </div>
         </div>
     </div>
-</div>
 @endsection
